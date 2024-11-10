372ad97d69f92ceb5ff95bb9e5fd0241a9f3bb2c
natalijapav <nataljija@hotmail.com>
2024-11-10 20:47:41 +0100

    initial commit

--- obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a367b420c9c34a51a4f083ae27f45cb40d18d6a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
